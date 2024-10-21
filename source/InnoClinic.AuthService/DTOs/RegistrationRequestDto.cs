c18a4ca296816398e8f84f6fc1b36d65888a6406
Dageron <Dageron12345@mail.ru>
2024-10-21 23:28:01 +0200

    Removes all changes

--- source/InnoClinic.AuthService/DTOs/RegistrationRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InnoClinic.AuthService.DTOs
-{
-    public class RegistrationRequestDto
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string PhoneNumber { get; set; }
-    }
-}
--- source/InnoClinic.AuthService/Services/Interfaces/ITokenService.cs
+++
@@ -1,8 +0,0 @@
-﻿using InnoClinic.AuthService.Entities;
-
-namespace InnoClinic.AuthService.Services.Interfaces;
-
-public interface ITokenService
-{
-    string GenerateToken(ApplicationUser applicationUser, IEnumerable<string> roles);
-}
